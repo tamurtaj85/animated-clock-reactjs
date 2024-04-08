f9ba9d27294a118d5023bfeb5555f0a08d4ae7d8
Tamur Ahmed <tamurahmed@Tamurs-MacBook-Air.local>
2024-04-08 14:42:49 +0500

    added 12h format support

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import anime from 'animejs/lib/anime.es.js';
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import './App.css';
 
-const calculateDisplacement = (value) => 150 - value * (15 + 8);
+const calculateDisplacement = (value) => {
+  /**
+   *
+   * 150 is the translated value 150px
+   * 15 is the font size i.e. 15px
+   * 8 is the gap value i.e. 4px top and bottom, so 8px
+   * adjust these values accordingly
+   *
+   * */
+  return 150 - value * (15 + 8);
+};
 
 function App() {
+  const [is12hFormatChecked, setis12hFormatChecked] = useState(false);
+
   const splitDigits = useCallback((value, sliderSectionId) => {
     const digit1 = Math.floor(value / 10);
     const digit2 = value % 10;
@@ -35,30 +47,6 @@ function App() {
       duration: 10000,
       easing: 'spring()',
     });
-
-    // anime({
-    //   targets: '#hour-2nd-digit-container',
-    //   keyframes: [
-    //     { translateY: 250 },
-    //     ...Array.from({ length: container.childNodes.length }).reduce(
-    //       (acc, _curr, index) => {
-    //         return [
-    //           ...acc,
-    //           {
-    //             // 250 translated value for the divs,
-    //             // 60+20 is text size + gap between each number
-    //             translateY: 250 - index * (60 + 20),
-    //             delay: delay1s,
-    //             easing: 'spring()',
-    //           },
-    //         ];
-    //       },
-    //       []
-    //     ),
-    //   ],
-    //   duration: 10000,
-    //   loop: true,
-    // });
   };
 
   const highlightDigit = (sliderId, val1, val2) => {
@@ -72,29 +60,9 @@ function App() {
       digit.classList.toggle('activeDigit', index == val1);
     });
 
-    // anime({
-    //   targets: `#${sliderCol1.childNodes[val1].id}`,
-    //   width: '120px',
-    //   height: '120px',
-    //   easing: 'easeInOutQuad',
-    //   duration: 400,
-    //   direction: 'alternate',
-    //   // loop: true,
-    // });
-
     sliderCol2.childNodes.forEach((digit, index) => {
       digit.classList.toggle('activeDigit', index == val2);
     });
-
-    // anime({
-    //   targets: `#${sliderCol2.childNodes[val2].id}`,
-    //   width: '120px',
-    //   height: '120px',
-    //   easing: 'easeInOutQuad',
-    //   duration: 400,
-    //   direction: 'alternate',
-    //   // loop: true,
-    // });
   };
 
   useEffect(() => {
@@ -107,90 +75,112 @@ function App() {
       //   s: currentDateTime.getSeconds(),
       // });
 
+      const hours = currentDateTime.getHours();
+      const formattedHours =
+        is12hFormatChecked && hours >= 13 ? hours - 12 : hours;
+
       // setting the time values and formatting them into relevant portions
-      splitDigits(currentDateTime.getHours(), 'hour-section');
+      splitDigits(formattedHours, 'hour-section');
       splitDigits(currentDateTime.getMinutes(), 'minute-section');
       splitDigits(currentDateTime.getSeconds(), 'second-section');
     }, 1000);
 
     return () => clearInterval(intervalId);
-  }, [splitDigits]);
+  }, [splitDigits, is12hFormatChecked]);
+
+  const handleOnChangeFormat = (event) => {
+    setis12hFormatChecked(event.target.checked);
+  };
 
   return (
     <div className="">
       <div className="mainContainer">
-        {/*  */}
-        {/* hours section */}
-        {/*  */}
-        <section id="hour-section" className="digitSections">
-          <div id="hour-1st-digit-container" className="digitContainer">
-            <div id="hourCol1-0">0</div>
-            <div id="hourCol1-1">1</div>
-            <div id="hourCol1-2">2</div>
+        <div>
+          <p>Time format: HH:MM:SS</p>
+          <div>
+            <input
+              type="checkbox"
+              name="timeFormat"
+              checked={is12hFormatChecked}
+              onChange={handleOnChangeFormat}
+            />
+            <label htmlFor="timeFormat">12H Format?</label>
           </div>
-          <div id="hour-2nd-digit-container" className="digitContainer">
-            <div id="hourCol2-0">0</div>
-            <div id="hourCol2-1">1</div>
-            <div id="hourCol2-2">2</div>
-            <div id="hourCol2-3">3</div>
-            <div id="hourCol2-4">4</div>
-            <div id="hourCol2-5">5</div>
-            <div id="hourCol2-6">6</div>
-            <div id="hourCol2-7">7</div>
-            <div id="hourCol2-8">8</div>
-            <div id="hourCol2-9">9</div>
-          </div>
-        </section>
-        {/*  */}
-        {/* minutes section */}
-        {/*  */}
-        <section id="minute-section" className="digitSections">
-          <div id="minute-1st-digit-container" className="digitContainer">
-            <div id="minuteCol1-0">0</div>
-            <div id="minuteCol1-1">1</div>
-            <div id="minuteCol1-2">2</div>
-            <div id="minuteCol1-3">3</div>
-            <div id="minuteCol1-4">4</div>
-            <div id="minuteCol1-5">5</div>
-          </div>
-          <div id="minute-2nd-digit-container" className="digitContainer">
-            <div id="minuteCol2-0">0</div>
-            <div id="minuteCol2-1">1</div>
-            <div id="minuteCol2-2">2</div>
-            <div id="minuteCol2-3">3</div>
-            <div id="minuteCol2-4">4</div>
-            <div id="minuteCol2-5">5</div>
-            <div id="minuteCol2-6">6</div>
-            <div id="minuteCol2-7">7</div>
-            <div id="minuteCol2-8">8</div>
-            <div id="minuteCol2-9">9</div>
-          </div>
-        </section>
-        {/*  */}
-        {/* seconds section */}
-        {/*  */}
-        <section id="second-section" className="digitSections">
-          <div id="second-1st-digit-container" className="digitContainer">
-            <div id="secondCol1-0">0</div>
-            <div id="secondCol1-1">1</div>
-            <div id="secondCol1-2">2</div>
-            <div id="secondCol1-3">3</div>
-            <div id="secondCol1-4">4</div>
-            <div id="secondCol1-5">5</div>
-          </div>
-          <div id="second-2nd-digit-container" className="digitContainer">
-            <div id="secondCol2-0">0</div>
-            <div id="secondCol2-1">1</div>
-            <div id="secondCol2-2">2</div>
-            <div id="secondCol2-3">3</div>
-            <div id="secondCol2-4">4</div>
-            <div id="secondCol2-5">5</div>
-            <div id="secondCol2-6">6</div>
-            <div id="secondCol2-7">7</div>
-            <div id="secondCol2-8">8</div>
-            <div id="secondCol2-9">9</div>
-          </div>
-        </section>
+        </div>
+        <div className="digitContainer">
+          {/*  */}
+          {/* hours section */}
+          {/*  */}
+          <section id="hour-section" className="digitSections">
+            <div id="hour-1st-digit-container" className="digitSliders">
+              <div id="hourCol1-0">0</div>
+              <div id="hourCol1-1">1</div>
+              <div id="hourCol1-2">2</div>
+            </div>
+            <div id="hour-2nd-digit-container" className="digitSliders">
+              <div id="hourCol2-0">0</div>
+              <div id="hourCol2-1">1</div>
+              <div id="hourCol2-2">2</div>
+              <div id="hourCol2-3">3</div>
+              <div id="hourCol2-4">4</div>
+              <div id="hourCol2-5">5</div>
+              <div id="hourCol2-6">6</div>
+              <div id="hourCol2-7">7</div>
+              <div id="hourCol2-8">8</div>
+              <div id="hourCol2-9">9</div>
+            </div>
+          </section>
+          {/*  */}
+          {/* minutes section */}
+          {/*  */}
+          <section id="minute-section" className="digitSections">
+            <div id="minute-1st-digit-container" className="digitSliders">
+              <div id="minuteCol1-0">0</div>
+              <div id="minuteCol1-1">1</div>
+              <div id="minuteCol1-2">2</div>
+              <div id="minuteCol1-3">3</div>
+              <div id="minuteCol1-4">4</div>
+              <div id="minuteCol1-5">5</div>
+            </div>
+            <div id="minute-2nd-digit-container" className="digitSliders">
+              <div id="minuteCol2-0">0</div>
+              <div id="minuteCol2-1">1</div>
+              <div id="minuteCol2-2">2</div>
+              <div id="minuteCol2-3">3</div>
+              <div id="minuteCol2-4">4</div>
+              <div id="minuteCol2-5">5</div>
+              <div id="minuteCol2-6">6</div>
+              <div id="minuteCol2-7">7</div>
+              <div id="minuteCol2-8">8</div>
+              <div id="minuteCol2-9">9</div>
+            </div>
+          </section>
+          {/*  */}
+          {/* seconds section */}
+          {/*  */}
+          <section id="second-section" className="digitSections">
+            <div id="second-1st-digit-container" className="digitSliders">
+              <div id="secondCol1-0">0</div>
+              <div id="secondCol1-1">1</div>
+              <div id="secondCol1-2">2</div>
+              <div id="secondCol1-3">3</div>
+              <div id="secondCol1-4">4</div>
+              <div id="secondCol1-5">5</div>
+            </div>
+            <div id="second-2nd-digit-container" className="digitSliders">
+              <div id="secondCol2-0">0</div>
+              <div id="secondCol2-1">1</div>
+              <div id="secondCol2-2">2</div>
+              <div id="secondCol2-3">3</div>
+              <div id="secondCol2-4">4</div>
+              <div id="secondCol2-5">5</div>
+              <div id="secondCol2-6">6</div>
+              <div id="secondCol2-7">7</div>
+              <div id="secondCol2-8">8</div>
+              <div id="secondCol2-9">9</div>
+            </div>
+          </section>
+        </div>
       </div>
     </div>
   );
